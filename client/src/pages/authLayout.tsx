90fe0354bf603a30e6a7c998916287d1326c6141
Prasanth <prasanthsampath2005@gmail.com>
2026-04-18 05:58:03 +0530

    fix: add image

--- client/src/pages/authLayout.tsx
+++ client/src/pages/authLayout.tsx
@@ -2,8 +2,12 @@ import { Outlet } from "react-router-dom"
 export default function AuthLayout(){
     return(
         <div className="h-screen w-full flex items-center justify-center">
-            <div className="hidden lg:block w-1/2 bg-black h-full">
-                
+            <div className="hidden lg:flex w-1/2 bg-black h-full items-center justify-center">
+                <img
+                    src="/hero.png"
+                    alt="Glowing abstract EasyHire visual"
+                    className="relative z-10 w-full max-w-[760px] object-contain drop-shadow-[0_0_40px_rgba(131,255,86,0.08)]"
+                />
             </div>
             <div className="w-full lg:w-1/2 flex items-center justify-center px-2 md:px-0 h-full">
                 <Outlet />
